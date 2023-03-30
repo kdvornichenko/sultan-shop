172c28bb5fe902dfa7a639f33dd07882be090b41
kdvornichenko <gglade999@gmail.com>
2023-03-30 22:51:27 +0300

    fix

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -9,11 +9,14 @@ import ProductPage from './pages/ProductPage'
 export default () => (
 	<SkeletonTheme baseColor="#EDEDED" highlightColor="#DADADA">
 		<Routes>
-			<Route path="./" element={<Navigate to="./catalog" />} />
-			<Route path="./catalog" element={<Catalog />} />
-			<Route path="./control-panel" element={<ControlPanel />} />
-			<Route path="./catalog/:productId" element={<ProductPage />} />
-			<Route path="./cart" element={<Cart />} />
+			<Route
+				path="/sultan-shop/"
+				element={<Navigate to="/sultan-shop/catalog" />}
+			/>
+			<Route path="/sultan-shop/catalog" element={<Catalog />} />
+			<Route path="/sultan-shop/control-panel" element={<ControlPanel />} />
+			<Route path="/sultan-shop/catalog/:productId" element={<ProductPage />} />
+			<Route path="/sultan-shop/cart" element={<Cart />} />
 		</Routes>
 	</SkeletonTheme>
 )
