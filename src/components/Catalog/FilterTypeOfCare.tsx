c998085516df605696162b761e067c2cebce591a
kdvornichenko <gglade999@gmail.com>
2023-03-31 22:37:59 +0300

    fix

--- src/components/Catalog/FilterTypeOfCare.tsx
+++ src/components/Catalog/FilterTypeOfCare.tsx
@@ -1,44 +1,24 @@
 import { filterData } from 'data/filter.data'
 import { IFilterItem } from 'models'
-import { ChangeEvent, FC, useEffect, useState } from 'react'
+import { ChangeEvent, FC } from 'react'
 import { useSelector } from 'react-redux'
-import { useDispatch } from 'react-redux'
 
-import {
-	selectFilteredByTypeProducts,
-	selectProductsData,
-} from '@/store/reducers/productsReducer'
-import { setFilteredByTypeProducts } from '@/store/slices/productsSlice'
+import { selectProductsData } from '@/store/reducers/productsReducer'
 
 import { FiltersState } from '@/types/filter.types'
 
-const FilterTypeOfCare: FC<{
+const Filter: FC<{
 	filters: FiltersState
 	setFilters: (filters: FiltersState) => void
 }> = ({ filters, setFilters }) => {
-	const dispatch = useDispatch()
 	const filteredTypesOfCareData = [...filterData]
 	const products = useSelector(selectProductsData)
-	const [filteredProducts, setFilteredProducts] = useState(products)
-	const filtProd = useSelector(selectFilteredByTypeProducts)
 
 	const handleFilterChange = (event: ChangeEvent<HTMLInputElement>) => {
 		const value = event.target.value
 		const checked = event.target.checked
 		setFilters({ ...filters, [value]: checked })
 	}
-
-	useEffect(() => {
-		const filtered = products.filter((product) => {
-			return product.typeOfCare.some((type) => filters[type])
-		})
-		setFilteredProducts(filtered)
-	}, [filters, products])
-
-	useEffect(() => {
-		dispatch(setFilteredByTypeProducts(filteredProducts))
-		console.log(filteredProducts)
-	}, [filteredProducts, dispatch])
 
 	return (
 		<>
@@ -58,4 +38,4 @@ const FilterTypeOfCare: FC<{
 	)
 }
 
-export default FilterTypeOfCare
+export default Filter
